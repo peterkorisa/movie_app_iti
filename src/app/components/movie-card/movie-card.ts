f86fc88e733ff87579ee54d6b697ef20e887beb2
amrhosny11 <amr.hosny24.8.12@gmail.com>
2025-08-20 10:29:42 +0300

    refactor:Rating circle

--- src/app/components/movie-card/movie-card.ts
+++ src/app/components/movie-card/movie-card.ts
@@ -1,13 +1,14 @@
-import { DatePipe, DecimalPipe, NgClass, PercentPipe } from '@angular/common'; 
+import { DatePipe, DecimalPipe, NgClass, NgStyle, PercentPipe } from '@angular/common'; 
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Watchlistservice } from '../../services/watchlistservice';
 import { RouterLink } from '@angular/router';
 
 
+
 @Component({
   selector: 'app-movie-card',
-  imports: [NgClass ,DecimalPipe ,FormsModule,DatePipe,RouterLink],
+  imports: [NgClass, DecimalPipe, FormsModule, DatePipe, RouterLink,NgStyle],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.css'
 })
