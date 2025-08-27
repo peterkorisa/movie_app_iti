53e3dc3c4112f9b59fc7eeac0dda1a138b720989
Zeyad Tarek <zeyad952004@gmail.com>
2025-08-27 23:47:04 +0300

    Add route in movielist/ to add required page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,11 @@ import { Moviedetails } from './components/moviedetails/moviedetails';
 import { SearchBar } from './components/search-bar/search-bar';
 
 export const routes: Routes = [
+  { path: 'movielist/:page_number', component: Movielist },
   { path: 'movielist', component: Movielist },
-  { path: '', redirectTo: 'movielist', pathMatch: 'full' },
   { path: 'search/:query', component: SearchBar },
   { path: 'watchlist', component: WatchlistCard },
   { path: 'movie/:id', component: Moviedetails },
+  { path: '', redirectTo: 'movielist', pathMatch: 'full' },
   { path: '**', redirectTo: '' }
 ];
--- src/app/components/movielist/movielist.ts
+++ src/app/components/movielist/movielist.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Movie } from '../../services/movie';
 import { RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { MovieCard } from '../movie-card/movie-card';
 import { FormsModule, NgModel } from '@angular/forms';
 @Component({
@@ -10,13 +11,23 @@ import { FormsModule, NgModel } from '@angular/forms';
   styleUrl: './movielist.css',
 })
 export class Movielist {
+  page_number: string = "1";
   movies: any[] = [];
   query = '';
 
-  constructor(private movieService: Movie) {}
+  constructor(
+    private movieService: Movie,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.movieService.getNowPlaying().subscribe((res: any) => {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('page_number');
+      this.page_number = id || "1";
+    });
+
+    console.log(this.page_number);
+    this.movieService.getNowPlaying(this.page_number).subscribe((res: any) => {
       this.movies = res.results;
     });
   }
