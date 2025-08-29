dfaa439d11348158eea3e963eff51ad57e8a74ab
amrhosny11 <amr.hosny24.8.12@gmail.com>
2025-08-29 17:47:56 +0300

    add watchlist in local storage

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Watchlistservice } from '../../services/watchlistservice';
 
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
 })
 export class Navbar {
+  constructor(public watchlistService: Watchlistservice) {}
 }
--- src/app/services/watchlistservice.ts
+++ src/app/services/watchlistservice.ts
@@ -1,46 +1,42 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Watchlistservice {
-
   private watchlist: any[] = [];
-  
+  constructor() {
+    const saved = localStorage.getItem('watchlist');
+    this.watchlist = saved ? JSON.parse(saved) : [];
+  }
+  private saveToLocalStorage() {
+    localStorage.setItem('watchlist', JSON.stringify(this.watchlist));
+  }
   getWatchlist() {
     return this.watchlist;
   }
 
   addMovie(movie: any) {
-    if (!this.watchlist.find(m => m.id === movie.id)) {
+    if (!this.watchlist.find((m) => m.id === movie.id)) {
       this.watchlist.push(movie);
+      this.saveToLocalStorage();
     }
   }
 
   removeMovie(movieId: number) {
-    this.watchlist = this.watchlist.filter(m => m.id !== movieId);
+    this.watchlist = this.watchlist.filter((m) => m.id !== movieId);
+    this.saveToLocalStorage();
   }
 
   isInWatchlist(movieId: number): boolean {
-    return this.watchlist.some(m => m.id === movieId);
+    return this.watchlist.some((m) => m.id === movieId);
   }
-  
-   toggleWatchlist(movie: any) {
+
+  toggleWatchlist(movie: any) {
     if (this.isInWatchlist(movie.id)) {
       this.removeMovie(movie.id);
     } else {
       this.addMovie(movie);
-    }
-  }
-
-  // New method to update a movie's details in the watchlist
-   updateMovie(updatedMovie: any) {
-    const index = this.watchlist.findIndex(m => m.id === updatedMovie.id);
-    if (index !== -1) {
-      // Create a shallow copy of the array and update the movie
-      const newWatchlist = [...this.watchlist];
-      newWatchlist[index] = updatedMovie;
-      this.watchlist = newWatchlist;
     }
   }
 }
